a1781739e0cb87f5e11e55fd29b225dcb3b373a6
math <mathewkipchumba2@gmail.com>
2024-07-14 06:39:59 +0300

    created folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="h1-bold">Next js 14, we&apos;re coming</h1>
+      <h1 className="h1-bold">Next js 14, we&apos;re coming , 15 soon!</h1>
     </div>
   );
 }
