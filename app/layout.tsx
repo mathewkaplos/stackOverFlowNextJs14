5fab6270e5b3da82532094f3975ef8b23167d9e7
mathewkaplos <mathewkipchumba2@gmail.com>
2024-07-14 10:45:50 +0300

    add assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,15 +9,48 @@ import {
 } from "@clerk/nextjs";
 import "./globals.css";
 
+import { Inter, Space_Grotesk } from "next/font/google";
+
+import type { Metadata } from "next";
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-space_Grotesk",
+});
+
+export const metadata: Metadata = {
+  title: "DevFlow",
+  description:
+    "A community driven platform for asking and answeing programming questions",
+  icons: {
+    // icon: "/assets/images/site-logo.svg",
+    icon: "/public/assets/images/site-logo.svg",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: "primary-gradient",
+          footerActionLink: "primary-text-gradient hover:text-primary-500",
+        },
+      }}
+    >
       <html lang="en">
-        <body>
+        <body className={`${inter.variable} ${spaceGrotesk.className}`}>
           <SignedOut>
             <SignInButton />
           </SignedOut>
